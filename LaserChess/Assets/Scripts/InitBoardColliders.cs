cb35edf591a48a559484de318e9a1eae6477136c
Alex Lamar <alawi@vt.edu>
2014-01-18 05:27:19 -0500

    Added logic to return the center of a grid that the user clicks
    Removed the 8x8 collision box logic because it won't work at all, what we
    need is a way to determine the center of the grid that the user clicked
    and this gives us that functionality

--- LaserChess/Assets/Scripts/InitBoardColliders.cs
+++ LaserChess/Assets/Scripts/InitBoardColliders.cs
@@ -8,6 +8,14 @@ public class InitBoardColliders : MonoBehaviour {
 	void Start () {
 		float xUnscaledSize = renderer.bounds.extents.x;
 		float yUnscaledSize = renderer.bounds.extents.y;
+
+		Debug.Log ("unscaled size X: " + xUnscaledSize + " Y: " + yUnscaledSize);
+
+		float xTransform = transform.position.x;
+		float yTransform = transform.position.y;
+
+		Debug.Log ("transform X: " + xTransform + " Y: " + yTransform);
+
 		//gives the length of one half of the board
 		float xScaledSize = (xUnscaledSize / 8);
 		float yScaledSize = (yUnscaledSize / 8);
@@ -16,14 +24,10 @@ public class InitBoardColliders : MonoBehaviour {
 		float xStep = xScaledSize / 4;
 		float yStep = yScaledSize / 4;
 
-		//create an 8x8 grid of colliders, corresponding to the chess squares on the board
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
+
 				BoxCollider2D collider = (BoxCollider2D) gameObject.AddComponent (typeof(BoxCollider2D));
-				collider.center = new Vector3(-xScaledSize + (xStep / 2) + xStep * i, -yScaledSize + (yStep / 2) + yStep * j, 0);
-				collider.size = new Vector3 (xStep, yStep, 0);
-			}
-		}
+				collider.center = new Vector3(0, 0, 0);
+				collider.size = new Vector3 (xScaledSize * 2, xScaledSize * 2, 0);
 	}
 	
 	// Update is called once per frame
@@ -33,6 +37,35 @@ public class InitBoardColliders : MonoBehaviour {
 
 	void OnMouseDown ()
 	{
-		Debug.Log ("Clicked at mouse X: " + Input.mousePosition.x + " Y: " + Input.mousePosition.y);
-	} 
+		Vector2 grid = getCenterOfGridAtMouse ();
+		Debug.Log ("Center X: " + grid.x + " Y: " + grid.y);
+	}
+
+	Vector2 getCenterOfGridAtMouse() {
+		float xUnscaledSize = renderer.bounds.extents.x;
+		float yUnscaledSize = renderer.bounds.extents.y;
+		//take into account the chessboard's transform scale by dividing by 8
+		float xScaledSize = (xUnscaledSize / 8);
+		float yScaledSize = (yUnscaledSize / 8);
+
+		////////////////////////////////////////
+		Vector3 pointer = Camera.main.ScreenToWorldPoint(Input.mousePosition );
+		
+		float rawX = pointer.x;
+		float rawY = pointer.y;
+		
+		float ratio = xScaledSize / 4;
+
+		//Gives us coordinates in an even (-4, 4) scale
+		float scaledX = rawX / 8 / ratio;
+		float scaledY = rawY / 8 / ratio;
+		//which we use to calculate the position of the innermost edge of the grid at the mouse position
+		float floorX = Mathf.Floor (scaledX);
+		float floorY = Mathf.Floor (scaledY);
+		//add 0.5 to get the center of the grid...
+		floorX += 0.5f;
+		floorY += 0.5f;
+		//and return it with the original scale
+		return new Vector2 (floorX * ratio, floorY * ratio);
+	}
 }
