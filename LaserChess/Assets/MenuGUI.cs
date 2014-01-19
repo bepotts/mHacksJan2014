ea8c0e592c9acbebb0f971205026168b7a23a238
ObeliskIsHuge <ObeliskIsHuge@gmail.com>
2014-01-19 09:12:35 -0500

    Maybe? Final commit

---
+++ LaserChess/Assets/MenuGUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuGUI : MonoBehaviour {
+
+    void onGUI()
+    {
+        GUI.Box(new Rect(10 , 10 , 100 , 90 ) , "LASER CHESS!!!");
+    }
+
+}
